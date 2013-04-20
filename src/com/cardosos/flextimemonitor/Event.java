61caf3cdcc26ec3f83f5bb914b2be119003c3728
Oscar Cardoso <oscar.cardoso@gameloft.com>
2013-04-20 14:13:39 -0700

    Added EventAdapter.
    Modified Event class to reflect on EventAdapter.
    Modified listitem_item_row to stop using RelativeLayout attributes.

--- src/com/cardosos/flextimemonitor/Event.java
+++ src/com/cardosos/flextimemonitor/Event.java
@@ -8,6 +8,20 @@ public class Event {
 	private String type = " ";
 	public static final String CHECK_IN = "check_in";
 	public static final String CHECK_OUT = "check_out";
+	private int icon;
+	private String title;
+
+	public Event() {
+		super();
+		this.time = 0;
+		this.type = " ";
+	}
+	
+	public Event(long time, String type){
+		super();
+		this.time = time;
+		this.type = type;
+	}
 
 	public long getId() {
 		return id;
@@ -38,5 +52,22 @@ public class Event {
 	public String toString() {
 		//return type + " in " + DateFormat.format("dd/MM kk:mm:ss", time);
 		return type + ": (" + Long.toString(time) + ") - " + DateFormat.format("kk:mm:ss dd/MM", time);
+	}
+
+	public String getTitle() {
+		this.title = (String) DateFormat.format("kk:mm:ss dd/MM", time);
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getIcon() {
+		return icon;
+	}
+
+	public void setIcon(int icon) {
+		this.icon = icon;
 	}
 }
