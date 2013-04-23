16663d8fddd18d8d707b0fed8359936e6d3bd463
Oscar Cardoso <oscar.cardoso@gameloft.com>
2013-04-23 13:18:22 -0700

    Add fixed Time for HOUR, MINUTE, DAY and for MAX_FLEX_HOURS.
    Add todaysTime and encapsulating methods.
    Add getHourString to get a string of 3 chars that shows the hours in a long.
    
    Signed-off-by: Oscar Cardoso <darkson.xd@gmail.com>

--- src/com/cardosos/flextimemonitor/TimeManager.java
+++ src/com/cardosos/flextimemonitor/TimeManager.java
@@ -3,6 +3,11 @@ package com.cardosos.flextimemonitor;
 public class TimeManager{
 
 	private long lastCheckIn = 0;
+	private long todaysTime = 0;
+	public static long MAX_FLEX_HOURS = 10;
+	public static long DAY = 86400000;
+	public static long HOUR = 3600000;
+	public static long MINUTE = 60000;
 
 	public TimeManager(){
 	}
@@ -19,6 +24,15 @@ public class TimeManager{
 		minutes = minutes % 60;
 		return String.format("%03d:%02d:%02d", hours, minutes, seconds);
 	}
+	
+	public static String getHourString(long time){
+		int seconds = (int) (time / 1000);
+		int minutes = seconds / 60;
+		seconds = seconds % 60;
+		int hours = minutes / 60;
+		minutes = minutes % 60;
+		return String.format("%03d", hours);	
+	}
 
 	public void setLastCheckIn(long lastCheckIn){
 		this.lastCheckIn = lastCheckIn;
@@ -26,5 +40,13 @@ public class TimeManager{
 
 	public long getLastCheckIn(){
 		return lastCheckIn;
+	}
+
+	public long getTodaysTime() {
+		return todaysTime;
+	}
+
+	public void setTodaysTime(long todaysTime) {
+		this.todaysTime = todaysTime;
 	}
 }
