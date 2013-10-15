a215c36cb0f6bd7038286eb611750407a092b9c0
Oscar Cardoso <oscar.cardoso@gameloft.com>
2013-10-15 11:32:02 -0700

    Format constant definitions.

--- src/com/cardosos/flextimemonitor/Day.java
+++ src/com/cardosos/flextimemonitor/Day.java
@@ -15,15 +15,15 @@ public class Day {
 	 */
 
 	public static int STATE_OUT_IN_TIME 	= 0;
-	public static int STATE_IN_IN_TIME 	= 1;
-	public static int STATE_OUT_ABSENT 	= 2;
-	public static int STATE_IN_ABSENT 	= 3;
-	public static int STATE_OUT_OVERTIME = 4;
+	public static int STATE_IN_IN_TIME 		= 1;
+	public static int STATE_OUT_ABSENT 		= 2;
+	public static int STATE_IN_ABSENT 		= 3;
+	public static int STATE_OUT_OVERTIME 	= 4;
 	public static int STATE_IN_OVERTIME 	= 5;
-	public static int STATE_OUT_LUNCH 	= 6;
-	public static int STATE_IN_LUNCH 	= 7;
-	public static int STATE_OUT_WEEKEND = 8;
-	public static int STATE_IN_WEEKEND 	= 9;
+	public static int STATE_OUT_LUNCH 		= 6;
+	public static int STATE_IN_LUNCH 		= 7;
+	public static int STATE_OUT_WEEKEND 	= 8;
+	public static int STATE_IN_WEEKEND 		= 9;
 
 	public Day(){
 	}
