197ce594f407f726f03536f875849800561fd872
Oscar Cardoso <oscar.cardoso@gameloft.com>
2013-11-29 12:28:35 -0800

    Added states for a day off, a reposition day and a delayed day.

--- src/com/cardosos/flextimemonitor/Day.java
+++ src/com/cardosos/flextimemonitor/Day.java
@@ -14,16 +14,22 @@ public class Day {
 	 *
 	 */
 
-	public static final int STATE_OUT_IN_TIME 	= 0;
-	public static final int STATE_IN_IN_TIME 	= 1;
-	public static final int STATE_OUT_ABSENT 	= 2;
-	public static final int STATE_IN_ABSENT 	= 3;
-	public static final int STATE_OUT_OVERTIME 	= 4;
-	public static final int STATE_IN_OVERTIME 	= 5;
-	public static final int STATE_OUT_LUNCH 	= 6;
-	public static final int STATE_IN_LUNCH 		= 7;
-	public static final int STATE_OUT_WEEKEND 	= 8;
-	public static final int STATE_IN_WEEKEND 	= 9;
+	public static final int STATE_OUT_IN_TIME 		= 0;
+	public static final int STATE_IN_IN_TIME 		= 1;
+	public static final int STATE_OUT_ABSENT 		= 2;
+	public static final int STATE_IN_ABSENT 		= 3;
+	public static final int STATE_OUT_OVERTIME 		= 4;
+	public static final int STATE_IN_OVERTIME 		= 5;
+	public static final int STATE_OUT_LUNCH 		= 6;
+	public static final int STATE_IN_LUNCH 			= 7;
+	public static final int STATE_OUT_WEEKEND 		= 8;
+	public static final int STATE_IN_WEEKEND 		= 9;
+	public static final int STATE_OUT_DAYOFF    	= 10;//counting time in a dayoff
+	public static final int STATE_IN_DAYOFF			= 11;
+	public static final int STATE_OUT_REPOSITION	= 12;//counting time in a reposition day such as saturday
+	public static final int STATE_IN_REPOSITION		= 13;
+	public static final int STATE_OUT_DELAY			= 14;//counting time properly in a delayed arrival day
+	public static final int STATE_IN_DELAY			= 15;
 
 	public Day(){
 	}
